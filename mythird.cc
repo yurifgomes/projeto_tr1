8c26f1ffff619a7b637bf8aceed18e53505d6b1c
Yuri <yurigomes963@gmail.com>
2016-12-08 21:28:08 -0200

    fim dos comentarios

--- mythird.cc
+++ mythird.cc
@@ -107,23 +107,33 @@ main (int argc, char *argv[])
   // Esse metodo determina o typo de algoritmo utilizado para controle. Algoritmo AARF
   wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
 
+  // cARACTERISTICAS DO MAC. 
   WifiMacHelper mac;
+  
+  // IMPORTANTE: acho que aqui é criado o modelo 802.11. Cria-se um objeto ns-3-ssid. Pagina 63 da documentação. 
   Ssid ssid = Ssid ("ns-3-ssid");
+  // STA = Station? "a station (STA) is a device that has the capability to use the 802.11 protocol."
   mac.SetType ("ns3::StaWifiMac",
                "Ssid", SsidValue (ssid),
                "ActiveProbing", BooleanValue (false));
-
+  
+  // Aqui ele termina de criar os nodes wifi.
   NetDeviceContainer staDevices;
   staDevices = wifi.Install (phy, mac, wifiStaNodes);
 
+  // Nodes criados, aqui é iniciada a configuração do AP.
   mac.SetType ("ns3::ApWifiMac",
                "Ssid", SsidValue (ssid));
 
+  // Cria um app device e instala tudo!
   NetDeviceContainer apDevices;
   apDevices = wifi.Install (phy, mac, wifiApNode);
 
+  // Objeto que vai dar "mobilidade" aos nós. Bem legal lol
+  // A ideia é que os nos que não sejam AP fiquem andando por ai sem rumo na vida. 
   MobilityHelper mobility;
 
+  // Espalhando os nodes pelo grid. Tamanho do grid suficiente? 
   mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
                                  "MinX", DoubleValue (0.0),
                                  "MinY", DoubleValue (0.0),
@@ -132,52 +142,67 @@ main (int argc, char *argv[])
                                  "GridWidth", UintegerValue (3),
                                  "LayoutType", StringValue ("RowFirst"));
 
+  // Movimentos aleatorios dos nodes.
   mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
                              "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+  // Instalação desse modelo nos nós wifi.                           
   mobility.Install (wifiStaNodes);
 
+  // Aqui ele deixa o AP fixo no grid, sem se movimentar
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (wifiApNode);
 
+  // Criação dos protocolos
   InternetStackHelper stack;
   stack.Install (csmaNodes);
   stack.Install (wifiApNode);
   stack.Install (wifiStaNodes);
 
+  // Cria os endereços no modelo ipv4
   Ipv4AddressHelper address;
 
+  // Designa a rede 10.1.1.0 para os dois nós p2p
   address.SetBase ("10.1.1.0", "255.255.255.0");
   Ipv4InterfaceContainer p2pInterfaces;
   p2pInterfaces = address.Assign (p2pDevices);
 
+  // Designa a rede 10.1.2.0 para os nós CSMA
   address.SetBase ("10.1.2.0", "255.255.255.0");
   Ipv4InterfaceContainer csmaInterfaces;
   csmaInterfaces = address.Assign (csmaDevices);
 
+  // Designa a rede 10.1.3.0 para os dispositivos wifi e AP
   address.SetBase ("10.1.3.0", "255.255.255.0");
   address.Assign (staDevices);
   address.Assign (apDevices);
 
+  // Server echo vai no nó mais a direita na topologia. Seria um nó adicional? São 8 nós e ele cria no nono?
   UdpEchoServerHelper echoServer (9);
 
   ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
   serverApps.Start (Seconds (1.0));
   serverApps.Stop (Seconds (10.0));
-
+  
+  // Echo Client vai no ultimo STA (station node?) criado. De novo o numero 9. Pesquisar!
   UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
   echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
   echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
   echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
+  // O echo client aponta para o server da rede CSMA
   ApplicationContainer clientApps = 
     echoClient.Install (wifiStaNodes.Get (nWifi - 1));
   clientApps.Start (Seconds (2.0));
   clientApps.Stop (Seconds (10.0));
 
+
+  // Enable internetworking. Necessario pq? Não sei, mas tem que estar enable.
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
+  // A simulação roda forever and ever, precisa parar em um tempo. 
   Simulator::Stop (Seconds (10.0));
 
+  // Permite visualizar o trafego. Conceito: Pcap Tracing. Pesquisar. 
   if (tracing == true)
     {
       pointToPoint.EnablePcapAll ("third");
@@ -185,6 +210,7 @@ main (int argc, char *argv[])
       csma.EnablePcap ("third", csmaDevices.Get (0), true);
     }
 
+  // Roda e encerra
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;
